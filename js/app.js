16792367aed6f94cecdcb50b752bf4b8d9fa934b
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-11-24 09:50:27 +0500

    script async, defer

--- js/app.js
+++ js/app.js
@@ -42,27 +42,42 @@ let newUser = {
 
 // fetch("https://facebook.com/", { mode: "no-cors" });
 
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.headers.token = "token1";
+// axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+// axios.defaults.headers.token = "token1";
 
-axios.post("/users", newUser).then((res) => {
-  console.log(res.data);
-});
+// axios.post("/users", newUser).then((res) => {
+//   console.log(res.data);
+// });
 
-axios.get("/todos").then((res) => {
-  console.log(res.data);
-});
+// axios.get("/todos").then((res) => {
+//   console.log(res.data);
+// });
 
-axios.post(
-  "https://reqres.in/api/login",
-  {
-    email: "sdf@df.com",
-    password: "dfsdf",
-    token: "sdfsd",
-  },
-  {
-    headers: {
-      token: "token2",
-    },
-  }
-);
+// axios
+//   .post(
+//     "https://reqres.in/api/login",
+//     {
+//       email: "eve.holt@reqres.in",
+//       password: "",
+//       token: "sdfsd",
+//     },
+//     {
+//       headers: {
+//         token: "token2",
+//       },
+//     }
+//   )
+//   .catch((err) => {
+//     if (err.response?.status >= 400 && err.response?.status < 500) {
+//       console.log(err.response.status, err.response?.data.error);
+//       alert(err.response?.data.error);
+//     }
+//   });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log(document.querySelector("p"));
+//   document.querySelector("h1").style.color = "red";
+// });
+
+// console.log(document.querySelector("p"));
+// document.querySelector("h1").style.color = "red";
